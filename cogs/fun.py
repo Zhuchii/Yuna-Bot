5c13b1db1d177ece47db8e37e15d049fe8cd507d
Zhuchii <147560218+Zhuchii@users.noreply.github.com>
2024-04-29 13:22:04 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -32,27 +32,12 @@ ball_responses = [
 ]
 
 
-tenor_api_key = 'AIzaSyAKW1k0uuI26qFAJhM2Kj5oTQVVu27K7kc'
-
 class fun(commands.Cog):
   
   
     def __init__(self, bot):
         self.bot = bot
         self.gif_cache = {}
-        
-        # Precachear los GIFs por carpeta
-        base_gif_folder = 'fun/gif'
-        subfolders = os.listdir(base_gif_folder)  # Lista de carpetas dentro de 'fun/gif'
-
-        # Cachea todos los GIFs en cada subcarpeta
-        for subfolder in subfolders:
-            folder_path = os.path.join(base_gif_folder, subfolder)
-            if os.path.isdir(folder_path):
-                # Carga todos los GIFs de esta carpeta en la caché
-                self.gif_cache[subfolder] = [
-                    os.path.join(folder_path, gif) for gif in os.listdir(folder_path) if gif.endswith(".gif")
-                ]
     
     
         @bot.tree.command(
@@ -135,16 +120,21 @@ class fun(commands.Cog):
         @bot.command()
         async def kiss(ctx, arg1=None):  
             if arg1!=None:
-                author = ctx.author.id
+                author = ctx.author.id  
+                if arg1=='<@1225963290358714482>':
+                    message= 'w~wh~why did y-you k-ki-kissed me suddenly (//▽//)'
+                else:
+                    message=f'<@{author}> is kissing {arg1} (づ ￣ ³￣)づ♡'
+                
                 with open('fun/gif/kiss.txt', 'r') as f:
                     enlaces = [linea.strip() for linea in f]
                     enlace_aleatorio = random.choice(enlaces)
-                embed = discord.Embed(title="",description=f'<@{author}> is kissing {arg1} (づ ￣ ³￣)づ♡( ๑‾̀◡‾́)σ»', color=discord.Color.blue())
+                embed = discord.Embed(title="",description=message, color=discord.Color.blue())
                 embed.set_image(url=enlace_aleatorio)
                 embed.timestamp = datetime.datetime.now()
                 await ctx.send(embed=embed)
             else:
-                emoji_id = 1231279253581205554  # ID del emoji
+                emoji_id = 1231279253581205554
                 emoji = bot.get_emoji(emoji_id)
                 if emoji is not None:
                     await ctx.reply(f'You have to tag someone to do that {emoji}')
@@ -155,24 +145,65 @@ class fun(commands.Cog):
         async def poke(ctx, arg1=None):
             if arg1!=None:
                 author = ctx.author.id
-                hug_gifs = self.gif_cache.get("poke", [])
+                with open('fun/gif/poke.txt', 'r') as f:
+                    enlaces = [linea.strip() for linea in f]
                 
-                if hug_gifs:
-                    # Elige un GIF al azar de la caché
-                    gif_path = random.choice(hug_gifs)
+                if enlaces:
+                    enlace_aleatorio = random.choice(enlaces)
                 
-                # Crea un embed
-                embed = discord.Embed(title="",description=f'<@{author}> is trying to poke {arg1} ( ๑‾̀◡‾́)σ» so annoying!', color=discord.Color.blue())
-                embed.set_image(url=f"attachment://{os.path.basename(gif_path)}")
+                embed = discord.Embed(title ='', description= f'<@{author}> is annoying {arg1} ( ๑‾̀◡‾́)σ»', color=discord.Color.blue())
+                embed.set_image(url=enlace_aleatorio)
                 embed.timestamp = datetime.datetime.now()
-            # Envía el embed con el GIF adjunto
-                with open(gif_path, "rb") as file:
-                    await ctx.send(embed=embed, file=discord.File(file, filename=os.path.basename(gif_path)))
+                await ctx.send(embed=embed)
             else:
-                emoji_id = 1231279253581205554  # ID del emoji
+                emoji_id = 1231279253581205554 
                 emoji = bot.get_emoji(emoji_id)
                 if emoji is not None:
                     await ctx.reply(f'You have to tag someone to do that {emoji}')
                 else:
                     await ctx.reply('You have to tag someone to do that')
 
+        @bot.command()
+        async def confused(ctx):
+            author = ctx.author.id
+            message = f'<@{author}> is confused  ୧( ˵ ° ~ ° ˵ )୨'
+                
+            with open('fun/gif/confused.txt', 'r') as f:
+                enlaces = [linea.strip() for linea in f]
+                enlace_aleatorio = random.choice(enlaces)
+            
+            embed = discord.Embed(title="",description=message, color=discord.Color.blue())
+            embed.set_image(url=enlace_aleatorio)
+            embed.timestamp = datetime.datetime.now()
+            await ctx.send(embed=embed)
+
+        @bot.command()
+        async def happy(ctx):
+            author = ctx.author.id
+            message = f'<@{author}> is happy ٩(^ᗜ^ )و ´-'
+                
+            with open('fun/gif/happy.txt', 'r') as f:
+                enlaces = [linea.strip() for linea in f]
+                enlace_aleatorio = random.choice(enlaces)
+            embed = discord.Embed(title="",description=message, color=discord.Color.blue())
+            embed.set_image(url=enlace_aleatorio)
+            embed.timestamp = datetime.datetime.now()
+            await ctx.send(embed=embed)
+
+        @bot.command()
+        async def hi(ctx, arg1=None):
+            author = ctx.author.id
+            if arg1==None:
+                message=f'<@{author}> says hi!  ＼(＾▽＾)' 
+            elif arg1=='<@1225963290358714482>':
+                message= f'Hii! <@{author}>  o(≧∇≦o)'
+            else:
+                message=f'<@{author}> says hi to {arg1}! ٩(^ᴗ^)۶'
+            
+            with open('fun/gif/hi.txt', 'r') as f:
+                enlaces = [linea.strip() for linea in f]
+                enlace_aleatorio = random.choice(enlaces)
+            embed = discord.Embed(title="",description=message, color=discord.Color.blue())
+            embed.set_image(url=enlace_aleatorio)
+            embed.timestamp = datetime.datetime.now()
+            await ctx.send(embed=embed)
