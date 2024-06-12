3d6eb3383e75eb7caaf812caac630a60cb7ea6a7
Zhuchii <147560218+Zhuchii@users.noreply.github.com>
2024-06-12 15:02:47 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -20,16 +20,18 @@ class help(commands.Cog):
             canal=ctx.channel.id
             if arg1 is None:
                 embed = discord.Embed(title ='Commands', description= 'A list of all the commands', color= discord.Color.blue())
-                embed.add_field(name='', value='-------------------------\n', inline=False)
-                embed.add_field(name='music', value='Show all the music commands', inline=False)
-                embed.add_field(name='roleplay', value='some actions that you can send to do some roleplay with your friends (if you have any)', inline=False)
-                embed.add_field(name='fun', value='Show all the fun and minigames commands', inline=False)
+                embed.add_field(name='Choose a category\n', value='', inline=False)
+                embed.add_field(name='music', value='', inline=False)
+                embed.add_field(name='roleplay', value='', inline=False)
+                embed.add_field(name='fun', value='', inline=False)
+                embed.add_field(name='moderation', value='All the moderation commands', inline=False)
                 embed.set_thumbnail(url='https://i.pinimg.com/originals/2a/0a/2a/2a0a2a1021e29f96173e35bc17f5b326.gif')
                 embed.set_image(url='https://qph.cf2.quoracdn.net/main-qimg-1a5fff09f7f19543037efe0cf1dbf4b3')
+                embed.add_field(name='', value='\n\n\nNote: If your found any bug pls send me a DM to zhuchii and ill fix it.')
                 embed.timestamp = datetime.datetime.now()
                 await ctx.send(content=None, embed=embed)
             elif arg1=='music':
-                embed = discord.Embed(title ='Commads', description= 'A list of all the commands', color= discord.Color.blue())
+                embed = discord.Embed(title ='Music', description= 'A list of all the music commands', color= discord.Color.blue())
                 embed.add_field(name='play', value='Plays music (Using youtube links)\n', inline=False)
                 embed.add_field(name='stop', value='Stop playing songs\n', inline=False)
                 embed.add_field(name='skip', value='Skip the current song and play the next song in the playlist\n', inline=False)
@@ -43,11 +45,12 @@ class help(commands.Cog):
                 embed = discord.Embed(title ='Fun', description= 'A list of all the Fun commands', color= discord.Color.blue())
                 embed.add_field(name='wordle', value='Play the classic wordle game\n', inline=False)
                 embed.add_field(name='ttt', value='TIC TAC TOE minigame\n', inline=False)
+                embed.add_field(name='guess', value='Try to guess the anime character by an image\n', inline=False)
                 await ctx.send(content=None, embed=embed)
             elif arg1 == 'roleplay':
                 embed = discord.Embed(
                     title='All the roleplay actions',
-                    description='Here are all the roleplay actions that you can do. (if u dont have friends to kiss u can also tag me >.<)',
+                    description='Some actions that you can send to do some roleplay with your friends (if you have any)',
                     color=discord.Color.blue()
                 )
                 
@@ -55,6 +58,12 @@ class help(commands.Cog):
                 acciones_columna2 = ["**confused**", "**happy**", "**hi**", "**angry**", ""]
                 embed.add_field(name='', value="\n\n".join(acciones_columna1), inline=True)
                 embed.add_field(name='', value="\n\n".join(acciones_columna2), inline=True)
+                await ctx.send(embed=embed)
+            elif arg1=='moderation':
+                embed = discord.Embed(title ='moderation', description= 'A list of all the moderation commands', color= discord.Color.blue())
+                embed.add_field(name='kick', value='Kick an user from your server')
+                embed.add_field(name='ban', value='ban an user from your server')
+                embed.add_field(name='timeout', value='Timeout a member for a specified time')
                 await ctx.send(embed=embed)
             else:
                 await ctx.reply(f'Command {arg1} not recognized, use -help for a guide to all commands')
@@ -68,10 +77,12 @@ class help(commands.Cog):
             canal=ctx.channel.id
             if command is None:
                 embed = discord.Embed(title ='Commands', description= 'A list of all the commands', color= discord.Color.blue())
-                embed.add_field(name='', value='-------------------------\n', inline=False)
+                embed.add_field(name='Choose a category\n', value='', inline=False)
                 embed.add_field(name='music', value='Show all the music commands', inline=False)
                 embed.add_field(name='roleplay', value='some actions that you can send to do some roleplay with your friends (if you have any)', inline=False)
                 embed.add_field(name='fun', value='Show all the fun and minigames commands', inline=False)
+                embed.add_field(name='moderation', value='All the moderation commands', inline=False)
+                embed.add_field(name='', value='\n\n\nNote: If your found any bug pls send me a DM to zhuchii and ill fix it.')
                 embed.set_thumbnail(url='https://i.pinimg.com/originals/2a/0a/2a/2a0a2a1021e29f96173e35bc17f5b326.gif')
                 embed.set_image(url='https://qph.cf2.quoracdn.net/main-qimg-1a5fff09f7f19543037efe0cf1dbf4b3')
                 embed.timestamp = datetime.datetime.now()
@@ -104,6 +115,12 @@ class help(commands.Cog):
                 acciones_columna2 = ["**confused**", "**happy**", "**hi**", "**angry**", ""]
                 embed.add_field(name='', value="\n\n".join(acciones_columna1), inline=True)
                 embed.add_field(name='', value="\n\n".join(acciones_columna2), inline=True)
-                await ctx.send(embed=embed)
+                await ctx.response.send_message(content=None, embed=embed)
+            elif command=='fun':
+                embed = discord.Embed(title ='Fun', description= 'A list of all the Fun commands', color= discord.Color.blue())
+                embed.add_field(name='wordle', value='Play the classic wordle game\n', inline=False)
+                embed.add_field(name='ttt', value='TIC TAC TOE minigame\n', inline=False)
+                embed.add_field(name='guess', value='Try to guess the anime character by an image\n', inline=False)
+                await ctx.response.send_message(content=None, embed=embed)
             else:
                 await ctx.reply(f'Command {command} not recognized, use -help for a guide to all commands')
